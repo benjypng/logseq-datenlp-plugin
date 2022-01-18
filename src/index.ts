4a6cee8fc2bbab7eb6f21c77b7122689034d9f59
hkgnp <ben@pngs.cc>
2022-01-18 19:21:57 +0800

    added scheduled and deadline function

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const getDateForPage = (d: Date, preferredDateFormat: string) => {
   const getDate = d.getDate();
 
   if (preferredDateFormat === 'MMM do yyyy') {
-    return `${getMonth} ${getOrdinalNum(getDate)}, ${getYear}`;
+    return `[[${getMonth} ${getOrdinalNum(getDate)}, ${getYear}]]`;
   } else if (
     preferredDateFormat.includes('yyyy') &&
     preferredDateFormat.includes('MM') &&
@@ -33,59 +33,93 @@ const getDateForPage = (d: Date, preferredDateFormat: string) => {
     dateStr = dateStr.replace(/yyyy|dd|MM/gi, function (matched) {
       return mapObj[matched];
     });
-    return dateStr;
+    return `[[${dateStr}]]`;
   } else {
     return `[[${getMonth} ${getOrdinalNum(getDate)}, ${getYear}]]`;
   }
+};
+
+const parseDates = async (preferredDateFormat: string, parseType: string) => {
+  // Parse block content
+  const currBlock = await logseq.Editor.getCurrentBlock();
+
+  const blockContent = await logseq.Editor.getEditingBlockContent();
+
+  const parsedBlock = await Sherlock.parse(blockContent);
+
+  // Destructure
+  const { isAllDay, eventTitle, startDate, endDate } = parsedBlock;
+
+  if (startDate === null) {
+    logseq.App.showMsg('There are no dates to parse.');
+    return;
+  }
+
+  if (parseType === 'date') {
+    // Create properties
+    startDate !== null
+      ? await logseq.Editor.upsertBlockProperty(
+          currBlock.uuid,
+          'date',
+          getDateForPage(startDate, preferredDateFormat)
+        )
+      : '';
+
+    isAllDay !== true
+      ? await logseq.Editor.upsertBlockProperty(
+          currBlock.uuid,
+          'time',
+          startDate.toLocaleTimeString()
+        )
+      : '';
+  } else if (parseType === 'SCHEDULED' || parseType === 'DEADLINE') {
+    startDate !== null
+      ? await logseq.Editor.updateBlock(
+          currBlock.uuid,
+          `${blockContent}
+${parseType}: <${new Date(startDate)
+            .toLocaleDateString()
+            .split('/')
+            .reverse()
+            .join('-')} A ${
+            !isAllDay && new Date(startDate).toLocaleTimeString()
+          }>`
+        )
+      : '';
+  }
+
+  window.setTimeout(async () => {
+    const nextBlock = await logseq.Editor.insertBlock(currBlock.uuid, '', {
+      sibling: true,
+      before: false,
+    });
+
+    await logseq.Editor.editBlock(nextBlock.uuid);
+  }, 600);
 };
 
 const main = async () => {
   console.log('logseq-datenlp-plugin loaded');
 
   const userConfigs = await logseq.App.getUserConfigs();
-  const preferredDateFormat = userConfigs.preferredDateFormat;
+  const preferredDateFormat: string = userConfigs.preferredDateFormat;
 
   logseq.Editor.registerSlashCommand('parse dates', async () => {
     window.setTimeout(async () => {
-      // Parse block content
-      const currBlock = await logseq.Editor.getCurrentBlock();
+      parseDates(preferredDateFormat, 'date');
+    }, 600);
+  });
 
-      const blockContent = await logseq.Editor.getEditingBlockContent();
+  logseq.Editor.registerSlashCommand('parse scheduled', async () => {
+    window.setTimeout(async () => {
+      parseDates(preferredDateFormat, 'SCHEDULED');
+    }, 600);
+  });
 
-      const parsedBlock = await Sherlock.parse(blockContent);
-
-      // Destructure
-      const { isAllDay, eventTitle, startDate, endDate } = parsedBlock;
-
-      if (startDate === null) {
-        logseq.App.showMsg('There are no dates to parse.');
-        return;
-      }
-
-      // Create properties
-      startDate !== null
-        ? await logseq.Editor.upsertBlockProperty(
-            currBlock.uuid,
-            'date',
-            getDateForPage(startDate, preferredDateFormat)
-          )
-        : '';
-
-      isAllDay !== true
-        ? await logseq.Editor.upsertBlockProperty(
-            currBlock.uuid,
-            'time',
-            startDate.toLocaleTimeString()
-          )
-        : '';
-
-      const nextBlock = await logseq.Editor.insertBlock(currBlock.uuid, '', {
-        sibling: true,
-        before: false,
-      });
-
-      await logseq.Editor.editBlock(nextBlock.uuid);
-    }, 500);
+  logseq.Editor.registerSlashCommand('parse deadline', async () => {
+    window.setTimeout(async () => {
+      parseDates(preferredDateFormat, 'DEADLINE');
+    }, 600);
   });
 };
 
