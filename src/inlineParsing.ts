084788723deff581e37ee3e20d2bc7a75467457a
hkgnp <ben@pngs.cc>
2022-02-24 16:40:07 +0800

    added function to parse the day and to add the block on that da

--- src/inlineParsing.ts
+++ src/inlineParsing.ts
@@ -1,6 +1,10 @@
 import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
 import chrono from 'chrono-node';
-import { getDateForPage, getScheduledDeadlineDateDay } from 'logseq-dateutils';
+import {
+  getDateForPage,
+  getScheduledDeadlineDateDay,
+  getDateForPageWithoutBrackets,
+} from 'logseq-dateutils';
 import { autoParsing } from './autoParsing';
 import { semiAutoParsing } from './semiAutoParsing';
 
@@ -39,12 +43,41 @@ export const inlineParsing = async (
       const parsedDate = parsedStartObject.date();
 
       if (content.includes(`@from`)) {
-        newContent = content.replace(
-          content,
-          `${content.replace(`@from ${parsedText}`, '')}
-start-time:: ${parsedStartObject.date().toTimeString().substring(0, 5)}
-end-time:: ${parsedEndObject.date().toTimeString().substring(0, 5)}`
-        );
+        window.setTimeout(async () => {
+          const currBlock = await logseq.Editor.getCurrentBlock();
+          const currPage = await logseq.Editor.getPage(currBlock.page.id);
+
+          newContent = content.replace(
+            content,
+            `${content.replace(`@from ${parsedText}`, '')}
+  start-time:: ${parsedStartObject.date().toTimeString().substring(0, 5)}
+  end-time:: ${parsedEndObject.date().toTimeString().substring(0, 5)}`
+          );
+
+          if (
+            getDateForPageWithoutBrackets(
+              parsedDate,
+              logseq.settings.preferredDateFormat
+            ) !== currPage.originalName
+          ) {
+            logseq.App.pushState('page', {
+              name: getDateForPageWithoutBrackets(
+                parsedDate,
+                logseq.settings.preferredDateFormat
+              ),
+            });
+
+            const newPage = await logseq.Editor.getCurrentPage();
+
+            await logseq.Editor.insertBlock(newPage.name, newContent, {
+              isPageBlock: true,
+            });
+
+            await logseq.Editor.removeBlock(currBlock.uuid);
+
+            return;
+          }
+        }, 1);
       } else if (content.includes(`@${parsedText}`)) {
         newContent = content.replace(
           `@${parsedText}`,
