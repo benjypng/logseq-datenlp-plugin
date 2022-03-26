c41c01687c4927a56540539f11490eed9f951694
hkgnp <ben@pngs.cc>
2022-03-26 17:23:42 +0800

    Add support for complete task with date

--- src/index.tsx
+++ src/index.tsx
@@ -1,122 +1,149 @@
-import '@logseq/libs';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { parseDates, callback } from './parseDates';
-import { handleClosePopup } from './handleClosePopup';
+import "@logseq/libs";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { parseDates, callback } from "./parseDates";
+import { handleClosePopup } from "./handleClosePopup";
+import { getDateForPageWithoutBrackets } from "logseq-dateutils";
 
 const main = () => {
-  console.log('logseq-datenlp-plugin loaded');
+    console.log("logseq-datenlp-plugin loaded");
 
-  window.setTimeout(async () => {
-    const userConfigs = await logseq.App.getUserConfigs();
-    const preferredDateFormat: string = userConfigs.preferredDateFormat;
-    logseq.updateSettings({ preferredDateFormat: preferredDateFormat });
-    console.log(`Settings updated to ${preferredDateFormat}`);
+    window.setTimeout(async () => {
+        const userConfigs = await logseq.App.getUserConfigs();
+        const preferredDateFormat: string = userConfigs.preferredDateFormat;
+        logseq.updateSettings({ preferredDateFormat: preferredDateFormat });
+        console.log(`Settings updated to ${preferredDateFormat}`);
 
-    if (!logseq.settings.lang) {
-      logseq.updateSettings({
-        lang: '',
-      });
-    }
-  }, 3000);
-
-  //@ts-expect-error
-  const observer = new top.MutationObserver(callback);
-  observer.observe(top.document.getElementById('app-container'), {
-    attributes: false,
-    childList: true,
-    subtree: true,
-  });
-
-  // register keyboard
-  logseq.App.registerCommandPalette(
-    {
-      key: 'logseq-datenlp-plugin-autoparsing',
-      label: 'Toggle auto-parsing on/off',
-      keybinding: {
-        binding: 'a p',
-      },
-    },
-    () => {
-      if (!logseq.settings.auto) {
-        logseq.updateSettings({ auto: true });
-        logseq.updateSettings({ semiAuto: false });
-        logseq.App.showMsg('Auto parsing ON');
-      } else {
-        logseq.updateSettings({ auto: false });
-        logseq.App.showMsg('Auto parsing OFF');
-      }
-    }
-  );
-
-  logseq.App.registerCommandPalette(
-    {
-      key: 'logseq-datenlp-plugin-semi-autoparsing',
-      label: 'Toggle semi-auto-parsing on/off',
-      keybinding: {
-        binding: 's p',
-      },
-    },
-    () => {
-      if (!logseq.settings.semiAuto) {
-        logseq.updateSettings({ semiAuto: true });
-        logseq.updateSettings({ auto: false });
-        logseq.App.showMsg('Semi auto parsing ON');
-      } else {
-        logseq.updateSettings({ semiAuto: false });
-        logseq.App.showMsg('Semi auto parsing OFF');
-      }
-    }
-  );
-
-  // register command palette
-  logseq.App.registerCommandPalette(
-    { key: 'logseq-datenlp-plugin-gotodate', label: '@goto parsed date ' },
-    () => {
-      logseq.showMainUI();
-
-      document.addEventListener('keydown', (e: any) => {
-        if (e.keyCode !== 27) {
-          (document.querySelector('.search-field') as HTMLElement).focus();
+        if (!logseq.settings.lang) {
+            logseq.updateSettings({
+                lang: "",
+            });
         }
-      });
-    }
-  );
+    }, 3000);
 
-  handleClosePopup();
+    //@ts-expect-error
+    const observer = new top.MutationObserver(callback);
+    observer.observe(top.document.getElementById("app-container"), {
+        attributes: false,
+        childList: true,
+        subtree: true,
+    });
 
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('app')
-  );
+    // register keyboard
+    logseq.App.registerCommandPalette(
+        {
+            key: "logseq-datenlp-plugin-autoparsing",
+            label: "Toggle auto-parsing on/off",
+            keybinding: {
+                binding: "a p",
+            },
+        },
+        () => {
+            if (!logseq.settings.auto) {
+                logseq.updateSettings({ auto: true });
+                logseq.updateSettings({ semiAuto: false });
+                logseq.App.showMsg("Auto parsing ON");
+            } else {
+                logseq.updateSettings({ auto: false });
+                logseq.App.showMsg("Auto parsing OFF");
+            }
+        }
+    );
 
-  ///////////// REGISTER SLASH COMMANDS /////////////
-  logseq.Editor.registerSlashCommand('parse dates', async () => {
-    window.setTimeout(async () => {
-      parseDates(logseq.settings.preferredDateFormat, 'date');
-    }, 600);
-  });
+    logseq.App.registerCommandPalette(
+        {
+            key: "logseq-datenlp-plugin-semi-autoparsing",
+            label: "Toggle semi-auto-parsing on/off",
+            keybinding: {
+                binding: "s p",
+            },
+        },
+        () => {
+            if (!logseq.settings.semiAuto) {
+                logseq.updateSettings({ semiAuto: true });
+                logseq.updateSettings({ auto: false });
+                logseq.App.showMsg("Semi auto parsing ON");
+            } else {
+                logseq.updateSettings({ semiAuto: false });
+                logseq.App.showMsg("Semi auto parsing OFF");
+            }
+        }
+    );
 
-  logseq.Editor.registerSlashCommand('parse inline', async () => {
-    window.setTimeout(async () => {
-      parseDates(logseq.settings.preferredDateFormat, 'inline');
-    }, 600);
-  });
+    // register command palette
+    logseq.App.registerCommandPalette(
+        { key: "logseq-datenlp-plugin-gotodate", label: "@goto parsed date " },
+        () => {
+            logseq.showMainUI();
 
-  logseq.Editor.registerSlashCommand('parse scheduled', async () => {
-    window.setTimeout(async () => {
-      parseDates(logseq.settings.preferredDateFormat, 'SCHEDULED');
-    }, 600);
-  });
+            document.addEventListener("keydown", (e: any) => {
+                if (e.keyCode !== 27) {
+                    (
+                        document.querySelector(".search-field") as HTMLElement
+                    ).focus();
+                }
+            });
+        }
+    );
 
-  logseq.Editor.registerSlashCommand('parse deadline', async () => {
-    window.setTimeout(async () => {
-      parseDates(logseq.settings.preferredDateFormat, 'DEADLINE');
-    }, 600);
-  });
+    // add complete task and add date of completion
+    logseq.App.registerCommandPalette(
+        {
+            key: "logseq-datenlp-plugin-completetask",
+            label: "@complete task",
+            keybinding: {
+                binding: "",
+            },
+        },
+        async () => {
+            const currBlk = await logseq.Editor.getCurrentBlock();
+            if (!currBlk.content.startsWith("DONE")) {
+                let newContent = currBlk.content.replace("TODO", "DONE");
+                newContent =
+                    newContent +
+                    getDateForPageWithoutBrackets(
+                        new Date(),
+                        logseq.settings.preferredDateFormat
+                    );
+                await logseq.Editor.updateBlock(currBlk.uuid, newContent);
+            }
+        }
+    );
+
+    handleClosePopup();
+
+    ReactDOM.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+        document.getElementById("app")
+    );
+
+    ///////////// REGISTER SLASH COMMANDS /////////////
+    logseq.Editor.registerSlashCommand("parse dates", async () => {
+        window.setTimeout(async () => {
+            parseDates(logseq.settings.preferredDateFormat, "date");
+        }, 600);
+    });
+
+    logseq.Editor.registerSlashCommand("parse inline", async () => {
+        window.setTimeout(async () => {
+            parseDates(logseq.settings.preferredDateFormat, "inline");
+        }, 600);
+    });
+
+    logseq.Editor.registerSlashCommand("parse scheduled", async () => {
+        window.setTimeout(async () => {
+            parseDates(logseq.settings.preferredDateFormat, "SCHEDULED");
+        }, 600);
+    });
+
+    logseq.Editor.registerSlashCommand("parse deadline", async () => {
+        window.setTimeout(async () => {
+            parseDates(logseq.settings.preferredDateFormat, "DEADLINE");
+        }, 600);
+    });
 };
 
 logseq.ready(main).catch(console.error);
--- src/inlineParsing.ts
+++ src/inlineParsing.ts
@@ -1,126 +1,133 @@
-import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user';
-import chrono from 'chrono-node';
+import { BlockEntity } from "@logseq/libs/dist/LSPlugin.user";
+import chrono from "chrono-node";
 import {
-  getDateForPage,
-  getDateForPageWithoutBrackets,
-  getScheduledDeadlineDateDay,
-} from 'logseq-dateutils';
-import { autoParsing } from './autoParsing';
-import { semiAutoParsing } from './semiAutoParsing';
+    getDateForPage,
+    getDateForPageWithoutBrackets,
+    getScheduledDeadlineDateDay,
+} from "logseq-dateutils";
+import { autoParsing } from "./autoParsing";
+import { semiAutoParsing } from "./semiAutoParsing";
 
 export const inlineParsing = async (
-  currBlock: BlockEntity,
-  parseType: string
+    currBlock: BlockEntity,
+    parseType: string
 ) => {
-  const { content } = currBlock;
+    const { content } = currBlock;
 
-  // Check for auto parsing
-  let chronoBlock: any[];
+    // Check for auto parsing
+    let chronoBlock: any[];
 
-  // If language settings are set
-  if (logseq.settings.lang) {
-    chronoBlock = chrono[`${logseq.settings.lang}`].parse(content, new Date(), {
-      forwardDate: true,
-    });
+    // If language settings are set
+    if (logseq.settings.lang) {
+        chronoBlock = chrono[`${logseq.settings.lang}`].parse(
+            content,
+            new Date(),
+            {
+                forwardDate: true,
+            }
+        );
 
-    // If language settings are not set
-  } else {
-    chronoBlock = chrono.parse(content, new Date(), {
-      forwardDate: true,
-    });
-  }
+        // If language settings are not set
+    } else {
+        chronoBlock = chrono.parse(content, new Date(), {
+            forwardDate: true,
+        });
+    }
 
-  // If the content to be parsed has a valid time/date element
-  if (chronoBlock.length > 0) {
-    let newContent: string = '';
+    // If the content to be parsed has a valid time/date element
+    if (chronoBlock.length > 0) {
+        let newContent: string = "";
 
-    if (parseType === 'semiAuto') {
-      const { parsedText, parsedStartObject, parsedEndObject } =
-        semiAutoParsing(currBlock, chronoBlock);
+        if (parseType === "semiAuto") {
+            const { parsedText, parsedStartObject, parsedEndObject } =
+                semiAutoParsing(currBlock, chronoBlock);
 
-      if (parsedText === null || parsedStartObject === null) return;
+            if (parsedText === null || parsedStartObject === null) return;
 
-      const parsedDate = parsedStartObject.date();
+            const parsedDate = parsedStartObject.date();
 
-      if (content.includes(`@from`)) {
-        // Create item with required start and end times
-        newContent = content.replace(
-          content,
-          `${content.replace(`@from ${parsedText}`, '')}
+            if (content.includes(`@from`)) {
+                // Create item with required start and end times
+                newContent = content.replace(
+                    content,
+                    `${content.replace(`@from ${parsedText}`, "")}
 start-time:: ${parsedStartObject.date().toTimeString().substring(0, 5)}
 end-time:: ${parsedEndObject.date().toTimeString().substring(0, 5)}`
-        );
+                );
 
-        // Go to page
-        const pageName = getDateForPageWithoutBrackets(
-          parsedDate,
-          logseq.settings.preferredDateFormat
-        ).toLowerCase();
+                // Go to page
+                const pageName = getDateForPageWithoutBrackets(
+                    parsedDate,
+                    logseq.settings.preferredDateFormat
+                ).toLowerCase();
 
-        // logseq.App.pushState('page', { name: pageName });
+                // logseq.App.pushState('page', { name: pageName });
 
-        await logseq.Editor.insertBlock(pageName, newContent, {
-          isPageBlock: true,
-        });
+                await logseq.Editor.insertBlock(pageName, newContent, {
+                    isPageBlock: true,
+                });
 
-        await logseq.Editor.removeBlock(currBlock.uuid);
+                await logseq.Editor.removeBlock(currBlock.uuid);
 
-        return;
-      } else if (content.includes(`@${parsedText}`)) {
-        newContent = content.replace(
-          `@${parsedText}`,
-          getDateForPage(parsedDate, logseq.settings.preferredDateFormat)
-        );
-      } else if (content.includes(`%${parsedText}`)) {
-        newContent = content.replace(
-          content,
-          `${content.replace(`%${parsedText}`, '')}
+                return;
+            } else if (content.includes(`@${parsedText}`)) {
+                newContent = content.replace(
+                    `@${parsedText}`,
+                    getDateForPage(
+                        parsedDate,
+                        logseq.settings.preferredDateFormat
+                    )
+                );
+            } else if (content.includes(`%${parsedText}`)) {
+                newContent = content.replace(
+                    content,
+                    `${content.replace(`%${parsedText}`, "")}
 SCHEDULED: <${getScheduledDeadlineDateDay(parsedDate)}${
-            parsedStartObject.knownValues.hour !== undefined
-              ? ` ${parsedDate.toTimeString().substring(0, 5)}`
-              : ``
-          }>`
-        );
-      } else if (content.includes(`^${parsedText}`)) {
-        newContent = content.replace(
-          content,
-          `${content.replace(`^${parsedText}`, '')}
+                        parsedStartObject.knownValues.hour !== undefined
+                            ? ` ${parsedDate.toTimeString().substring(0, 5)}`
+                            : ``
+                    }>`
+                );
+            } else if (content.includes(`^${parsedText}`)) {
+                newContent = content.replace(
+                    content,
+                    `${content.replace(`^${parsedText}`, "")}
 DEADLINE: <${getScheduledDeadlineDateDay(parsedDate)}${
-            parsedStartObject.knownValues.hour !== undefined
-              ? ` ${parsedDate.toTimeString().substring(0, 5)}`
-              : ``
-          }>`
-        );
-      }
-    } else if (parseType === 'auto') {
-      newContent = autoParsing(currBlock, chronoBlock);
+                        parsedStartObject.knownValues.hour !== undefined
+                            ? ` ${parsedDate.toTimeString().substring(0, 5)}`
+                            : ``
+                    }>`
+                );
+            }
+        } else if (parseType === "auto") {
+            newContent = autoParsing(currBlock, chronoBlock);
+        }
+
+        if (!newContent) return;
+
+        // Account for if the block content contains BOTH @time and date/time to parse
+        if (newContent.includes("@time")) {
+            const nowTime = chrono
+                .parse("now", new Date(), { forwardDate: true })[0]
+                .start.date()
+                .toTimeString()
+                .substring(0, 5);
+            newContent = newContent.replace("@time", nowTime);
+        }
+
+        // UPDATE BLOCK WITH WHATEVER NEWCONTENT IS DERIVED FROM ABOVE
+        await logseq.Editor.updateBlock(currBlock.uuid, newContent);
     }
 
-    if (!newContent) return;
+    // Account for if the block content contains only @time
+    if (currBlock.content.includes("@time")) {
+        const nowTime = chrono
+            .parse("now", new Date(), { forwardDate: true })[0]
+            .start.date()
+            .toTimeString()
+            .substring(0, 5);
+        const newContent = currBlock.content.replace("@time", nowTime);
 
-    // Account for if the block content contains BOTH @time and date/time to parse
-    if (newContent.includes('@time')) {
-      const nowTime = chrono
-        .parse('now', new Date(), { forwardDate: true })[0]
-        .start.date()
-        .toTimeString()
-        .substring(0, 5);
-      newContent = newContent.replace('@time', nowTime);
+        await logseq.Editor.updateBlock(currBlock.uuid, newContent);
     }
-
-    // UPDATE BLOCK WITH WHATEVER NEWCONTENT IS DERIVED FROM ABOVE
-    await logseq.Editor.updateBlock(currBlock.uuid, newContent);
-  }
-
-  // Account for if the block content contains only @time
-  if (currBlock.content.includes('@time')) {
-    const nowTime = chrono
-      .parse('now', new Date(), { forwardDate: true })[0]
-      .start.date()
-      .toTimeString()
-      .substring(0, 5);
-    const newContent = currBlock.content.replace('@time', nowTime);
-
-    await logseq.Editor.updateBlock(currBlock.uuid, newContent);
-  }
 };
