76d1d3bed088cd8a57a864531d32fe1cf533c699
hkgnp <ben@pngs.cc>
2022-04-07 17:12:05 +0800

    Added support for all day events

--- src/inlineParsing.ts
+++ src/inlineParsing.ts
@@ -1,133 +1,134 @@
 import { BlockEntity } from "@logseq/libs/dist/LSPlugin.user";
 import chrono from "chrono-node";
 import {
-    getDateForPage,
-    getDateForPageWithoutBrackets,
-    getScheduledDeadlineDateDay,
+  getDateForPage,
+  getDateForPageWithoutBrackets,
+  getScheduledDeadlineDateDay,
 } from "logseq-dateutils";
 import { autoParsing } from "./autoParsing";
 import { semiAutoParsing } from "./semiAutoParsing";
 
 export const inlineParsing = async (
-    currBlock: BlockEntity,
-    parseType: string
+  currBlock: BlockEntity,
+  parseType: string
 ) => {
-    const { content } = currBlock;
+  const { content } = currBlock;
 
-    // Check for auto parsing
-    let chronoBlock: any[];
+  // Check for auto parsing
+  let chronoBlock: any[];
 
-    // If language settings are set
-    if (logseq.settings.lang) {
-        chronoBlock = chrono[`${logseq.settings.lang}`].parse(
-            content,
-            new Date(),
-            {
-                forwardDate: true,
-            }
-        );
+  // If language settings are set
+  if (logseq.settings.lang) {
+    chronoBlock = chrono[`${logseq.settings.lang}`].parse(content, new Date(), {
+      forwardDate: true,
+    });
 
-        // If language settings are not set
-    } else {
-        chronoBlock = chrono.parse(content, new Date(), {
-            forwardDate: true,
-        });
-    }
+    // If language settings are not set
+  } else {
+    chronoBlock = chrono.parse(content, new Date(), {
+      forwardDate: true,
+    });
+  }
 
-    // If the content to be parsed has a valid time/date element
-    if (chronoBlock.length > 0) {
-        let newContent: string = "";
+  // If the content to be parsed has a valid time/date element
+  if (chronoBlock.length > 0) {
+    let newContent: string = "";
 
-        if (parseType === "semiAuto") {
-            const { parsedText, parsedStartObject, parsedEndObject } =
-                semiAutoParsing(currBlock, chronoBlock);
+    if (parseType === "semiAuto") {
+      const { parsedText, parsedStartObject, parsedEndObject } =
+        semiAutoParsing(currBlock, chronoBlock);
 
-            if (parsedText === null || parsedStartObject === null) return;
+      if (parsedText === null || parsedStartObject === null) return;
 
-            const parsedDate = parsedStartObject.date();
+      const parsedDate = parsedStartObject.date();
 
-            if (content.includes(`@from`)) {
-                // Create item with required start and end times
-                newContent = content.replace(
-                    content,
-                    `${content.replace(`@from ${parsedText}`, "")}
+      if (content.includes(`@from`)) {
+        // Create item with required start and end times
+        newContent = content.replace(
+          content,
+          `${content.replace(`@from ${parsedText}`, "")}
 start-time:: ${parsedStartObject.date().toTimeString().substring(0, 5)}
 end-time:: ${parsedEndObject.date().toTimeString().substring(0, 5)}`
-                );
+        );
 
-                // Go to page
-                const pageName = getDateForPageWithoutBrackets(
-                    parsedDate,
-                    logseq.settings.preferredDateFormat
-                ).toLowerCase();
+        await logseq.Editor.insertBlock(currBlock.uuid, newContent, {
+          sibling: true,
+          before: true,
+        });
 
-                // logseq.App.pushState('page', { name: pageName });
+        await logseq.Editor.exitEditingMode();
 
-                await logseq.Editor.insertBlock(pageName, newContent, {
-                    isPageBlock: true,
-                });
-
-                await logseq.Editor.removeBlock(currBlock.uuid);
-
-                return;
-            } else if (content.includes(`@${parsedText}`)) {
-                newContent = content.replace(
-                    `@${parsedText}`,
-                    getDateForPage(
-                        parsedDate,
-                        logseq.settings.preferredDateFormat
-                    )
-                );
-            } else if (content.includes(`%${parsedText}`)) {
-                newContent = content.replace(
-                    content,
-                    `${content.replace(`%${parsedText}`, "")}
+        await logseq.Editor.removeBlock(currBlock.uuid);
+        return;
+      } else if (content.includes(`@${parsedText}`)) {
+        newContent = content.replace(
+          `@${parsedText}`,
+          getDateForPage(parsedDate, logseq.settings.preferredDateFormat)
+        );
+      } else if (content.includes(`%${parsedText}`)) {
+        newContent = content.replace(
+          content,
+          `${content.replace(`%${parsedText}`, "")}
 SCHEDULED: <${getScheduledDeadlineDateDay(parsedDate)}${
-                        parsedStartObject.knownValues.hour !== undefined
-                            ? ` ${parsedDate.toTimeString().substring(0, 5)}`
-                            : ``
-                    }>`
-                );
-            } else if (content.includes(`^${parsedText}`)) {
-                newContent = content.replace(
-                    content,
-                    `${content.replace(`^${parsedText}`, "")}
+            parsedStartObject.knownValues.hour !== undefined
+              ? ` ${parsedDate.toTimeString().substring(0, 5)}`
+              : ``
+          }>`
+        );
+      } else if (content.includes(`^${parsedText}`)) {
+        newContent = content.replace(
+          content,
+          `${content.replace(`^${parsedText}`, "")}
 DEADLINE: <${getScheduledDeadlineDateDay(parsedDate)}${
-                        parsedStartObject.knownValues.hour !== undefined
-                            ? ` ${parsedDate.toTimeString().substring(0, 5)}`
-                            : ``
-                    }>`
-                );
-            }
-        } else if (parseType === "auto") {
-            newContent = autoParsing(currBlock, chronoBlock);
-        }
-
-        if (!newContent) return;
-
-        // Account for if the block content contains BOTH @time and date/time to parse
-        if (newContent.includes("@time")) {
-            const nowTime = chrono
-                .parse("now", new Date(), { forwardDate: true })[0]
-                .start.date()
-                .toTimeString()
-                .substring(0, 5);
-            newContent = newContent.replace("@time", nowTime);
-        }
-
-        // UPDATE BLOCK WITH WHATEVER NEWCONTENT IS DERIVED FROM ABOVE
-        await logseq.Editor.updateBlock(currBlock.uuid, newContent);
+            parsedStartObject.knownValues.hour !== undefined
+              ? ` ${parsedDate.toTimeString().substring(0, 5)}`
+              : ``
+          }>`
+        );
+      }
+    } else if (parseType === "auto") {
+      newContent = autoParsing(currBlock, chronoBlock);
     }
 
-    // Account for if the block content contains only @time
-    if (currBlock.content.includes("@time")) {
-        const nowTime = chrono
-            .parse("now", new Date(), { forwardDate: true })[0]
-            .start.date()
-            .toTimeString()
-            .substring(0, 5);
-        const newContent = currBlock.content.replace("@time", nowTime);
+    if (!newContent) return;
 
-        await logseq.Editor.updateBlock(currBlock.uuid, newContent);
+    // Account for if the block content contains BOTH @time and date/time to parse
+    if (newContent.includes("@time")) {
+      const nowTime = chrono
+        .parse("now", new Date(), { forwardDate: true })[0]
+        .start.date()
+        .toTimeString()
+        .substring(0, 5);
+      newContent = newContent.replace("@time", nowTime);
     }
+
+    // UPDATE BLOCK WITH WHATEVER NEWCONTENT IS DERIVED FROM ABOVE
+    await logseq.Editor.updateBlock(currBlock.uuid, newContent);
+  } else if (content.includes("@allday")) {
+    // Create all day block
+    const newContent = `${content.replace("@allday", "")}
+start-time:: all-day`;
+
+    await logseq.Editor.insertBlock(currBlock.uuid, newContent, {
+      sibling: true,
+      before: true,
+    });
+
+    await logseq.Editor.exitEditingMode();
+
+    await logseq.Editor.removeBlock(currBlock.uuid);
+    return;
+  }
+
+  // Account for if the block content contains only @time
+  if (currBlock.content.includes("@time")) {
+    const nowTime = chrono
+      .parse("now", new Date(), { forwardDate: true })[0]
+      .start.date()
+      .toTimeString()
+      .substring(0, 5);
+    const newContent = currBlock.content.replace("@time", nowTime);
+
+    await logseq.Editor.updateBlock(currBlock.uuid, newContent);
+  }
 };
