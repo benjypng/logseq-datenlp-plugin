07e7b0865812d626c2255737c9a7ebd014595d65
hkgnp <ben@pngs.cc>
2022-04-14 15:10:26 +0800

    Refactoring small..

--- src/App.tsx
+++ src/App.tsx
@@ -4,65 +4,65 @@ import chrono from "chrono-node";
 import { getDateForPage } from "logseq-dateutils";
 
 const App = () => {
-    const [searchVal, setSearchVal] = useState("");
+  const [searchVal, setSearchVal] = useState("");
 
-    const handleForm = (e: any) => {
-        setSearchVal(e.target.value);
-    };
+  const handleForm = (e: any) => {
+    setSearchVal(e.target.value);
+  };
 
-    const handleSubmit = async (e: any) => {
-        if (e.keyCode === 13) {
-            const chronoBlock = chrono.parse(searchVal, new Date(), {
-                forwardDate: true,
-            });
+  const handleSubmit = async (e: any) => {
+    if (e.keyCode === 13) {
+      const chronoBlock = chrono.parse(searchVal, new Date(), {
+        forwardDate: true,
+      });
 
-            if (chronoBlock.length > 0) {
-                const startingDate = getDateForPage(
-                    chronoBlock[0].start.date(),
-                    logseq.settings.preferredDateFormat
-                );
+      if (chronoBlock.length > 0) {
+        const startingDate = getDateForPage(
+          chronoBlock[0].start.date(),
+          logseq.settings.preferredDateFormat
+        );
 
-                logseq.App.pushState("page", {
-                    name: startingDate.substring(2, startingDate.length - 2),
-                });
+        logseq.App.pushState("page", {
+          name: startingDate.substring(2, startingDate.length - 2),
+        });
 
-                setSearchVal("");
+        setSearchVal("");
 
-                const currPBT = await logseq.Editor.getCurrentPageBlocksTree();
+        const currPBT = await logseq.Editor.getCurrentPageBlocksTree();
 
-                if (currPBT.length > 0) {
-                    logseq.hideMainUI({ restoreEditingCursor: true });
-                } else {
-                    const currPage = await logseq.Editor.getCurrentPage();
-                    await logseq.Editor.insertBlock(currPage.name, "", {
-                        isPageBlock: true,
-                    });
+        if (currPBT.length > 0) {
+          logseq.hideMainUI({ restoreEditingCursor: true });
+        } else {
+          const currPage = await logseq.Editor.getCurrentPage();
+          await logseq.Editor.insertBlock(currPage.name, "", {
+            isPageBlock: true,
+          });
 
-                    logseq.hideMainUI({ restoreEditingCursor: true });
-                }
-            }
+          logseq.hideMainUI({ restoreEditingCursor: true });
         }
-    };
+      }
+    }
+  };
 
-    return (
-        <div
-            className="search-container flex justify-center border border-black"
-            tabIndex={-1}
-        >
-            <div className=" absolute top-10 bg-white rounded-lg p-3 w-1/3 border">
-                <input
-                    className="search-field appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    type="text"
-                    placeholder="E.g. tomorrow, 4th July, 6 months later"
-                    aria-label="Parse day date"
-                    name="searchVal"
-                    onChange={handleForm}
-                    value={searchVal}
-                    onKeyDown={(e) => handleSubmit(e)}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="search-container flex justify-center border border-black"
+      tabIndex={-1}
+    >
+      <div className="absolute top-10 bg-white rounded-lg p-3 w-1/3 border">
+        <input
+          className="search-field appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          type="text"
+          placeholder="E.g. tomorrow, 4th July, 6 months later"
+          aria-label="Parse day date"
+          name="searchVal"
+          onChange={handleForm}
+          value={searchVal}
+          onKeyDown={(e) => handleSubmit(e)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
