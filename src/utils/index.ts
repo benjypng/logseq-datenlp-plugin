6dcf35dfc84b9579aa71dffda8f17152361f8c58
hkgnp <47936793+hkgnp@users.noreply.github.com>
2023-10-18 19:51:47 +0800

    fix: cannot parse

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,3 +11,37 @@ export const handlePopup = () => {
     false,
   );
 };
+
+export const setSettings = async () => {
+  if (!logseq.settings) return;
+  const { dateChar, scheduledChar, deadlineChar } = logseq.settings;
+
+  // Check if the above has equivalence
+  if (
+    dateChar === scheduledChar ||
+    dateChar === deadlineChar ||
+    scheduledChar === deadlineChar
+  ) {
+    // Reset settings if the special chars have equivalence
+    logseq.updateSettings({
+      dateChar: "@",
+      scheduledChar: "%",
+      deadlineChar: "^",
+    });
+    await logseq.UI.showMsg(
+      "There are overlapping characters for the inline parsing character. Please re-check your settings",
+      "error",
+    );
+    return;
+  }
+
+  window.setTimeout(async () => {
+    //Save user configs in settings;
+    const preferredDateFormat: string = (await logseq.App.getUserConfigs())
+      .preferredDateFormat;
+    logseq.updateSettings({ preferredDateFormat: preferredDateFormat });
+    console.info(
+      `logseq-datenlp-plugin: Settings updated to ${preferredDateFormat}`,
+    );
+  }, 1500);
+};
