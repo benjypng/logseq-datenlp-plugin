1afb3eaf7cf49e9ddcce97921d1b30c4e9869dd8
Benjy <ben@pngs.cc>
2024-06-30 15:21:22 +0800

    chore: clean

--- src/features/parse/index.ts
+++ src/features/parse/index.ts
@@ -1,45 +1,42 @@
-import { BlockEntity } from "@logseq/libs/dist/LSPlugin.user";
-import * as chrono from "chrono-node";
-import { ParsedResult } from "chrono-node/dist/cjs";
+import { BlockEntity } from '@logseq/libs/dist/LSPlugin.user'
+import * as chrono from 'chrono-node'
+import { ParsedResult } from 'chrono-node/dist/cjs'
 
-import { manualParse } from "~/features/parse/manual";
-import { semiAutoParse } from "~/features/parse/semi-auto";
+import { manualParse } from '~/features/parse/manual'
+import { semiAutoParse } from '~/features/parse/semi-auto'
 
 export const checkIfUrl = (str: string): boolean => {
   switch (true) {
-    case str.includes("http"):
-      return true;
-    case str.includes("://"):
-      return true;
-    case str.includes("www"):
-      return true;
+    case str.includes('http'):
+      return true
+    case str.includes('://'):
+      return true
+    case str.includes('www'):
+      return true
     default:
-      return false;
+      return false
   }
-};
+}
 
-export const checkIfChronoObjHasTime = (
-  startObj: Partial<ParsedResult>,
-): string => {
-  // @ts-ignore
+//@ts-expect-error Type of startObj doesn't match
+export const checkIfChronoObjHasTime = (startObj): string => {
   if (startObj.knownValues.hour) {
-    // @ts-ignore
-    return ` ${startObj.date().toTimeString().substring(0, 5)}`;
+    return ` ${startObj.date().toTimeString().substring(0, 5)}`
   } else {
-    return "";
+    return ''
   }
-};
+}
 
 const handleMultipleParsedText = (
   chronoBlock: ParsedResult[],
   content: string,
   options?: { flag: string },
 ): string => {
-  let parsedStr = "";
+  let parsedStr = ''
   for (let i = 0; i < chronoBlock.length; i++) {
-    const parsedText = chronoBlock[i]!.text;
-    const parsedStart = chronoBlock[i]!.start.date();
-    const parsedEnd = chronoBlock[i]!.end?.date();
+    const parsedText = chronoBlock[i]!.text
+    const parsedStart = chronoBlock[i]!.start.date()
+    const parsedEnd = chronoBlock[i]!.end?.date()
     if (i === 0) {
       if (!options?.flag) {
         const str = semiAutoParse(
@@ -48,8 +45,8 @@ const handleMultipleParsedText = (
           parsedText,
           parsedStart,
           parsedEnd,
-        );
-        if (str !== "") parsedStr = str;
+        )
+        if (str !== '') parsedStr = str
       } else {
         const str = manualParse(
           options.flag,
@@ -57,12 +54,12 @@ const handleMultipleParsedText = (
           chronoBlock,
           parsedText,
           parsedStart,
-        );
-        if (str) parsedStr = str;
+        )
+        if (str) parsedStr = str
       }
     }
     if (i > 0) {
-      if (logseq.settings!.insertDateProperty) break;
+      if (logseq.settings!.insertDateProperty) break
       if (!options?.flag) {
         parsedStr = semiAutoParse(
           parsedStr,
@@ -70,7 +67,7 @@ const handleMultipleParsedText = (
           parsedText,
           parsedStart,
           parsedEnd,
-        );
+        )
       } else {
         parsedStr = manualParse(
           options.flag,
@@ -78,29 +75,28 @@ const handleMultipleParsedText = (
           chronoBlock,
           parsedText,
           parsedStart,
-        );
+        )
       }
     }
   }
-  return parsedStr;
-};
+  return parsedStr
+}
 
 export const inlineParsing = async (
   currBlock: BlockEntity,
   options?: { flag: string },
 ): Promise<string | undefined> => {
-  const { content } = currBlock;
-  //@ts-ignore
-  const chronoBlock: ParsedResult[] = chrono[logseq.settings!.lang].parse(
-    content,
-    new Date(),
-  );
-  if (!chronoBlock || !chronoBlock[0]) return "";
+  const { content } = currBlock
+  const { lang } = logseq.settings!
+
+  // @ts-expect-error Type doesn't match
+  const chronoBlock: ParsedResult[] = chrono[lang].parse(content, new Date())
+  if (!chronoBlock || !chronoBlock[0]) return ''
 
   if (chronoBlock.length === 1) {
-    const parsedText = chronoBlock[0].text;
-    const parsedStart = chronoBlock[0].start.date();
-    const parsedEnd = chronoBlock[0].end?.date();
+    const parsedText = chronoBlock[0].text
+    const parsedStart = chronoBlock[0].start.date()
+    const parsedEnd = chronoBlock[0].end?.date()
 
     if (!options?.flag) {
       return semiAutoParse(
@@ -109,7 +105,7 @@ export const inlineParsing = async (
         parsedText,
         parsedStart,
         parsedEnd,
-      );
+      )
     } else {
       return manualParse(
         options.flag,
@@ -117,13 +113,13 @@ export const inlineParsing = async (
         chronoBlock,
         parsedText,
         parsedStart,
-      );
+      )
     }
   } else {
     if (!options?.flag) {
-      return handleMultipleParsedText(chronoBlock, content, options);
+      return handleMultipleParsedText(chronoBlock, content, options)
     } else {
-      return handleMultipleParsedText(chronoBlock, content, options);
+      return handleMultipleParsedText(chronoBlock, content, options)
     }
   }
-};
+}
--- src/features/parse/semi-auto.ts
+++ src/features/parse/semi-auto.ts
@@ -1,12 +1,12 @@
-import { ParsedResult } from "chrono-node";
+import { ParsedResult } from 'chrono-node'
 import {
   getDateForPage,
   getDeadlineDateDay,
   getScheduledDateDay,
-} from "logseq-dateutils";
+} from 'logseq-dateutils'
 
-import * as parse from "~/features/parse/index";
-import { PluginSettings } from "~/settings/types";
+import * as parse from '~/features/parse/index'
+import { PluginSettings } from '~/settings/types'
 
 export const semiAutoParse = (
   content: string,
@@ -16,87 +16,87 @@ export const semiAutoParse = (
   parsedEnd: Date | undefined,
 ): string => {
   const { dateChar, scheduledChar, deadlineChar } =
-    logseq.settings! as Partial<PluginSettings>;
-  if (!dateChar || !scheduledChar || !deadlineChar) throw new Error();
+    logseq.settings! as Partial<PluginSettings>
+  if (!dateChar || !scheduledChar || !deadlineChar) throw new Error()
 
   // handle special characters in code
-  const backticksRx = /`(.*?)`/g;
-  if (backticksRx.exec(content)) return "";
+  const backticksRx = /`(.*?)`/g
+  if (backticksRx.exec(content)) return ''
 
   switch (true) {
-    case content.includes("@from"): {
-      content = content.replace("@from", "").replace(parsedText, "");
+    case content.includes('@from'): {
+      content = content.replace('@from', '').replace(parsedText, '')
       content = `${content}
       start-time:: ${parsedStart.toTimeString().substring(0, 5)}
-      end-time:: ${parsedEnd?.toTimeString().substring(0, 5)}`;
-      return content;
+      end-time:: ${parsedEnd?.toTimeString().substring(0, 5)}`
+      return content
     }
     case content.includes(dateChar): {
-      const checkTime = parse.checkIfChronoObjHasTime(chronoBlock[0]!.start);
+      const checkTime = parse.checkIfChronoObjHasTime(chronoBlock[0]!.start)
       content = content.replace(
         `${dateChar}${parsedText}`,
         `${getDateForPage(
           parsedStart,
           logseq.settings!.preferredDateFormat,
         )}${checkTime}`,
-      );
-      return content;
+      )
+      return content
     }
     case content.includes(scheduledChar) || content.includes(deadlineChar): {
-      if (scheduledChar === "NA" || deadlineChar === "NA") {
-        return content;
+      if (scheduledChar === 'NA' || deadlineChar === 'NA') {
+        return content
       }
       const scheduledOrDeadline = content.includes(scheduledChar)
-        ? "SCHEDULED"
-        : "DEADLINE";
-      content = content.replace(`${scheduledChar}${parsedText}`, "");
-      content = content.replace(`${deadlineChar}${parsedText}`, "");
+        ? 'SCHEDULED'
+        : 'DEADLINE'
+      content = content.replace(`${scheduledChar}${parsedText}`, '')
+      content = content.replace(`${deadlineChar}${parsedText}`, '')
 
       if (logseq.settings?.removeTime)
-        parsedStart = new Date(parsedStart.setHours(0, 0, 0, 0));
+        parsedStart = new Date(parsedStart.setHours(0, 0, 0, 0))
 
-      if (scheduledOrDeadline === "SCHEDULED") {
+      if (scheduledOrDeadline === 'SCHEDULED') {
         content = `${content}
-${getScheduledDateDay(parsedStart)}`;
+${getScheduledDateDay(parsedStart)}`
       } else {
         content = `${content}
-${getDeadlineDateDay(parsedStart)}`;
+${getDeadlineDateDay(parsedStart)}`
       }
-      return content;
+      return content
     }
     default: {
-      return "";
+      return ''
     }
   }
-};
+}
 
 const callback = async (mutationsList: MutationRecord[]): Promise<void> => {
   for (const m of mutationsList) {
     if (
-      m.type === "childList" &&
+      m.type === 'childList' &&
       m.removedNodes.length > 0 &&
       (m.removedNodes[0]! as HTMLElement).className ===
-        "editor-inner block-editor"
+        'editor-inner block-editor'
     ) {
       const uuid = (m.target as HTMLElement)
         .closest('div[id^="ls-block"]')
-        ?.getAttribute("blockid") as string;
-      const currBlock = await logseq.Editor.getBlock(uuid);
-      if (!currBlock) return;
+        ?.getAttribute('blockid') as string
+      const currBlock = await logseq.Editor.getBlock(uuid)
+      if (!currBlock) return
 
       // Execute inline parsing
-      const content = await parse.inlineParsing(currBlock);
-      if (content) await logseq.Editor.updateBlock(uuid, content);
+      const content = await parse.inlineParsing(currBlock)
+      if (content) await logseq.Editor.updateBlock(uuid, content)
     }
   }
-};
+}
 
 export const parseMutationObserver = (): void => {
-  //@ts-expect-error
-  const observer = new top!.MutationObserver(callback);
-  observer.observe(top?.document.getElementById("app-container"), {
+  //@ts-expect-error Mutation does not exist on window
+  const observer = new top!.MutationObserver(callback)
+  observer.observe(top?.document.getElementById('app-container'), {
     attributes: false,
     childList: true,
     subtree: true,
-  });
-};
+  })
+}
